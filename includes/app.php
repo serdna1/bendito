9edc6ec8ecde2f1839839875ebc7b16b01f9b337
serdna1 <andrescaot@gmail.com>
2024-01-22 16:51:15 +0100

    Prepare project for deploy

--- includes/app.php
+++ includes/app.php
@@ -1,9 +1,12 @@
-<?php 
+<?php
+
+use Model\ActiveRecord;
+require '../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 require 'functions.php';
 require 'database.php';
-require '../vendor/autoload.php';
 
 // // The same database connection is used by all models
-use Model\ActiveRecord;
 ActiveRecord::setDB($db);
--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,13 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', 'bendito_db');
+$db = mysqli_connect(
+	$_ENV['DB_HOST'],
+	$_ENV['DB_USER'],
+	$_ENV['DB_PASS'],
+	$_ENV['DB_NAME'],
+);
+
+$db->set_charset('utf8');
 
 if (!$db) {
 	echo "Error: Unnable to connect to MySQL.";
